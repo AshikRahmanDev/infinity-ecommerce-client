2a9609236032e4a2253e192b6f8e4a6ee5d7e399
MohammdAshik <mohammadashik797@gmail.com>
2023-02-14 21:43:58 +0600

    navigation bar added

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Banner = () => {
-  return <div>this is from banner</div>;
+  return (
+    <div className="absolute top-0 w-[100%] brown">this is from banner</div>
+  );
 };
 
 export default Banner;
--- src/Shared/Header.jsx
+++ src/Shared/Header.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { BiSearch } from "react-icons/bi";
+import { RiUserLine } from "react-icons/ri";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 
 const Header = () => {
   return (
-    <div>
-      <nav className="flex justify-between items-center h-16 w-[92%] mx-auto ">
+    <div className="bg-transparent">
+      <nav className="flex justify-between items-center h-20 w-[92%] mx-auto ">
         <div>
-          <h4>logo</h4>
+          <h4 className="theme-font text-4xl">Infinity</h4>
         </div>
-        <div></div>
+        <div>
+          <ul className="flex items-center">
+            <li className="mx-3">
+              <BiSearch className="text-2xl" />
+            </li>
+            <li className="mx-3">
+              <RiUserLine className="text-2xl" />
+            </li>
+            <li className="mx-3">
+              <HiOutlineShoppingCart className="text-2xl" />
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
   );
