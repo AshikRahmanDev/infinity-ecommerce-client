d9c4b9bc0a3c23abfa920b1d6662328d4a191092
MohammdAshik <mohammadashik797@gmail.com>
2023-02-18 21:12:25 +0600

    product quick view modal added

--- src/Shared/ProductCard.jsx
+++ src/Shared/ProductCard.jsx
@@ -1,23 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineHeart } from "react-icons/ai";
+import { MdOutlineViewInAr } from "react-icons/md";
 import "../App.css";
+import ProductModal from "./ProductModal";
 
 const ProductCard = ({ product }) => {
   const { picture, title, price } = product;
   const { img1 } = picture;
   return (
-    <div className="w-[250px] mx-auto">
-      <div className="img-container h-[300px] w-[250px]">
-        <img
-          className="product-img object-cover"
-          style={{ backgroundColor: "#f1f0ee" }}
-          src={img1}
-          alt=""
-        />
+    <div>
+      <div className="w-[290px] mx-auto">
+        <div className="img-container h-[300px] w-[290px]">
+          <img
+            className="product-img object-cover"
+            style={{ backgroundColor: "#f1f0ee" }}
+            src={img1}
+            alt=""
+          />
+          <div className="flex items-center justify-between visit absolute h-[50px] bg-white w-full">
+            <Link className="flex mx-1 items-center uppercase logo-font text-[13px] text-gray-400">
+              <AiOutlineHeart className="mr-2" /> add to wishlist
+            </Link>
+            <span className="mx-2 text-gray-400">|</span>
+            <label
+              htmlFor="product-modal"
+              className="flex h-full mx-1 items-center uppercase logo-font text-[13px] text-gray-400"
+            >
+              <MdOutlineViewInAr className="mr-2" /> Quick view
+            </label>
+          </div>
+        </div>
+        <div>
+          <Link className="my-2 text-sm logo-font font-semibold">{title}</Link>
+          <p className="logo-font text-red-500 mb-2">$ {price}.00</p>
+          <Link className="uppercase logo-font block w-[90%] mx-auto text-center text-gray-400 border border-gray-400 py-2">
+            add to cart
+          </Link>
+        </div>
       </div>
-      <div>
-        <h4 className="my-2 text-sm logo-font font-semibold">{title}</h4>
-        <p className="logo-font text-red-500">$ {price}.00</p>
-      </div>
+      <ProductModal />
     </div>
   );
 };
